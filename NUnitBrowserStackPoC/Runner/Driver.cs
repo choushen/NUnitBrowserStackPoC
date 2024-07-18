f90d7aa3c938c9cbaee132f3122af803466a883d
Jacob <jacob.mckenzie@nhs.net>
2024-07-18 14:47:22 +0100

    Created a config manager and updated the driver creation method

--- NUnitBrowserStackPoC/Runner/Driver.cs
+++ NUnitBrowserStackPoC/Runner/Driver.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using NUnitBrowserStackPoC.Utilities;
+using static NUnitBrowserStackPoC.Utilities.BrowserStackConfigManager;
 
 namespace NUnitBrowserStackPoC.Driver
 {
@@ -15,24 +18,47 @@ namespace NUnitBrowserStackPoC.Driver
 
         public static IOSDriver<IOSElement> driverIOS;
         public static  Local browserStackLocal = null;
+        public static string currentEnv = "UAT1";
+        public static Platform selectedPlatform;
 
         public static void CreateDriver()
         {
 
+
+            string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string bsYmlConfig = Path.Combine(projectDirectory, "browserstack.yml");
+
+            BrowserStackConfigManager config = new BrowserStackConfigManager().Load(bsYmlConfig);
+
             var appiumOptions = new AppiumOptions();
-            
+
+            switch(currentEnv)
+            {
+                case "UAT1":
+                    selectedPlatform = config.platforms.First(p => p.environment == "UAT1");
+                    break;
+                case "UAT2":
+                    selectedPlatform = config.platforms.First(p => p.environment == "UAT2");
+                    break;
+                default:
+                    selectedPlatform = config.platforms.First(p => p.environment == "UAT1"); 
+                    break;
+            }
+
+
+
             // Configure Appium driver
-            appiumOptions.AddAdditionalCapability("browserstack.user", "");
-            appiumOptions.AddAdditionalCapability("browserstack.key", "");
-            appiumOptions.AddAdditionalCapability("device", "iPad 10th");
-            appiumOptions.AddAdditionalCapability("os_version", "16");
-            appiumOptions.AddAdditionalCapability("project", "NUnitBrowserStackPoC");
-            appiumOptions.AddAdditionalCapability("app", "");
-            appiumOptions.AddAdditionalCapability("build", "Test Build");
-            appiumOptions.AddAdditionalCapability("name", "bstack-demo");
-            appiumOptions.AddAdditionalCapability("browserstack.local", "true");
-            appiumOptions.AddAdditionalCapability("browserstack.debug", "true");
-            appiumOptions.AddAdditionalCapability("browserstack.networkLogs", "true");
+            appiumOptions.AddAdditionalCapability("browserstack.user", config.userName);
+            appiumOptions.AddAdditionalCapability("browserstack.key", config.accessKey);
+            appiumOptions.AddAdditionalCapability("device", selectedPlatform.deviceName);
+            appiumOptions.AddAdditionalCapability("os_version", selectedPlatform.platformVersion);
+            appiumOptions.AddAdditionalCapability("project", config.projectName);
+            appiumOptions.AddAdditionalCapability("app", config.app);
+            appiumOptions.AddAdditionalCapability("build", config.buildName);
+            appiumOptions.AddAdditionalCapability("name", "test name");
+            appiumOptions.AddAdditionalCapability("browserstack.local", config.browserstackLocal);
+            appiumOptions.AddAdditionalCapability("browserstack.debug", config.debug);
+            appiumOptions.AddAdditionalCapability("browserstack.networkLogs", config.networkLogs);
 
             // Configure BS Local
             browserStackLocal = new Local();
---
+++ NUnitBrowserStackPoC/Utilities/BrowserStackConfigManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+
+namespace NUnitBrowserStackPoC.Utilities
+{
+    public class BrowserStackConfigManager
+    {
+
+        public string userName { get; set; }
+        public string accessKey { get; set; }
+        public string app { get; set; }
+        public Platform[] platforms { get; set; }
+        public bool browserstackLocal { get; set; }
+        public string buildName { get; set; }
+        public string projectName { get; set; }
+        public bool debug { get; set; }
+        public bool networkLogs { get; set; }
+
+
+        public class Platform
+        {
+            public string platformName { get; set; }
+            public string deviceName { get; set; }
+            public string platformVersion { get; set; }
+            public string environment { get; set; }
+        }
+
+
+        public BrowserStackConfigManager Load(string path)
+        {
+            var deserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+
+            using (var reader = new StreamReader(path))
+            {
+                return deserializer.Deserialize<BrowserStackConfigManager>(reader);
+            }
+
+        }
+
+
+    }
+}
